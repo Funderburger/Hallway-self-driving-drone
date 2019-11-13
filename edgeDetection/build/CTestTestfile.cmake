bea095ca6cd733d6ffdf880522a9737eed90f874
Funderburger <popmarianleontin@gmail.com>
2019-11-13 12:47:54 +0200

    deleting unnecessary files

--- edgeDetection/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tyfun/TutorialsAndOthers/edgeDetection/src
-# Build directory: /home/tyfun/TutorialsAndOthers/edgeDetection/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
